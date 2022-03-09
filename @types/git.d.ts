07ebd69a9498762f6d69576a86df6ea6386b57c8
estebandalelr <estebandalelr@gmail.com>
2022-03-09 17:17:59 -0600

    Give attribution

--- @types/git.d.ts
+++ @types/git.d.ts
@@ -1,7 +1,9 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+ *--------------------------------------------------------------------------------------------
+ Taken from: https://github.com/microsoft/vscode/blob/main/extensions/git/src/api/git.d.ts
+ */
 
 import { Event, Uri } from 'vscode';
 
