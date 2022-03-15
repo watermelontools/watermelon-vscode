2609a1ea486c1acf0b68e57596e201584dba9b5e
Juan <juands1903@gmail.com>
2022-03-14 22:27:52 -0500

    Delete function

--- src/utils/escapeFilePath.ts
+++
@@ -1,7 +0,0 @@
-function escapeFilePath(path:string| undefined){
-	if(path){
-		// $& means the whole matched string
-		return path.replace(/[. *\s+\ ?^${}()|[\]\\]/g, '\\$&');
-	}
-	else {return "";}
-}
