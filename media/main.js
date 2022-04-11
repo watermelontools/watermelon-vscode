70e97931404d107601cc974721f3b33624762216
Esteban Dalel R <estebandalelr@gmail.com>
2022-04-11 09:06:30 -0500

    Finished clamping code

--- media/main.js
+++ media/main.js
@@ -52,6 +52,20 @@ $(document).ready(function () {
       `);
     });
     hljs.highlightAll();
+    $("code").each(function(index, element) {
+    
+    // replace each with the clamped version and a see more button
+    if($(this).text().length > 100) {
+      $(this).addClass("clamp");
+      $(this).append("<button class='see-more'>See More</button>");
+    }
+    // now restore the text when the button was clicked
+    $(this).on("click", ".see-more", function() {
+      $(this).parent().removeClass("clamp");
+      $(this).remove();
+    }
+    );
+  });
   };
   const setLoading = () => {
     $("#ghHolder").append(`
