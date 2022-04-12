231518bcdc421a47d243328a8e1da39cb23d354d
estebandalelr <estebandalelr@gmail.com>
2022-04-12 14:29:34 -0500

    Add activation test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,6 +3,7 @@ import * as assert from "assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
+import sleep from "./utils/sleep";
 // import * as myExtension from '../../extension';
 
 suite("Extension Test Suite", () => {
@@ -18,6 +19,13 @@ suite("Extension Test Suite", () => {
   test("Extension is present", () => {
     assert.ok(
       vscode.extensions.getExtension("WatermelonTools.watermelon-tools")
+    );
+  });
+  // test that the extension is activated
+  test("Extension is activated", async() => {
+    await sleep(1500);
+    assert.ok(
+      vscode.extensions.getExtension("WatermelonTools.watermelon-tools")?.isActive
     );
   });
 });
