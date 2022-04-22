763ff48379b5a75864e61c9b7b0e1dacc6aae20f
estebandalelr <estebandalelr@gmail.com>
2022-04-22 16:14:48 -0500

    Added issue titles

--- media/main.js
+++ media/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
   const replaceIssueLinks = (text, repo_url) => {
     let repoLink = repo_url.replace("api.", "").replace("repos/", "");
     return text
-      .replace(/#([0-9]*)/gm, `<a href="${repoLink}/pull/$1">$&</a>`)
+      .replace(/#([0-9]*)/gm, `<a href="${repoLink}/pull/$1" title="View this issue on github">$&</a>`)
       .replaceAll(`&<a href="${repoLink}/pull/39">#39</a>;`, "'");
   };
 
