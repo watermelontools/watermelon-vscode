ac2df63eb6b3b2fc2c4e68dc344affeb4ed457d8
estebandalelr <estebandalelr@gmail.com>
2022-04-22 16:15:05 -0500

    Added tag titles

--- media/main.js
+++ media/main.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
     return text
       .replace(
         /\B@([a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9]))/gi,
-        `<a href="https://github.com/$&">$&</a>`.toLowerCase()
+        `<a href="https://github.com/$&" title="View this user on github">$&</a>`.toLowerCase()
       )
       .replaceAll("/@", "/");
   };
