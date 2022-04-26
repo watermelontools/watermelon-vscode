af32e106be3808da9478281c430298da58ac81b9
Esteban Dalel R <estebandalelr@gmail.com>
2022-04-26 17:43:34 -0500

    Fix regex up to year 2000

--- src/utils/getBlameAuthors.ts
+++ src/utils/getBlameAuthors.ts
@@ -9,7 +9,7 @@ export default async function getAuthorArray(
     currentlyOpenTabfilePath || "."
   );
   let blameLines = blame?.split("\n").slice(startLine, endLine + 1);
-  let authors = blameLines.map((line: String) => line.split(/[(]|\s\s/)[1]);
+  let authors = blameLines.map((line: String) => line.split(/[(]|\s\s|202|201|200/)[1]);
 
   let authorFreq = authors.reduce(
     (acc: { [x: string]: any }, curr: string | number) => {
