92fab675f59677aa8fb448c282aa81da15aa268b
Esteban Dalel R <estebandalelr@gmail.com>
2022-04-26 10:26:01 -0500

    Sort authors per frequency

--- src/utils/getBlameAuthors.ts
+++ src/utils/getBlameAuthors.ts
@@ -1,18 +1,25 @@
 export default async function getAuthorArray(
-    startLine: number,
-    endLine: number,
-    currentlyOpenTabfilePath: string| undefined,
-    gitAPI: any | undefined
-  ): Promise<string[]> {
-    let arrayOfAuthors: string[] = [];
-    let blame = await gitAPI?.repositories[0].blame(
-      currentlyOpenTabfilePath || "."
-    );
-  let blameLines= blame?.split("\n").slice(startLine, endLine + 1);
-  let authors= blameLines.map((line:String)=>line.split(/[(]|\s\s\s/)[1])
-  
-  console.log(authors)
-  
-    return authors;
-  }
-  
+  startLine: number,
+  endLine: number,
+  currentlyOpenTabfilePath: string | undefined,
+  gitAPI: any | undefined
+): Promise<string[]> {
+  let arrayOfAuthors: string[] = [];
+  let blame = await gitAPI?.repositories[0].blame(
+    currentlyOpenTabfilePath || "."
+  );
+  let blameLines = blame?.split("\n").slice(startLine, endLine + 1);
+  let authors = blameLines.map((line: String) => line.split(/[(]|\s\s/)[1]);
+
+  let authorFreq = authors.reduce(
+    (acc: { [x: string]: any }, curr: string | number) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    },
+    {}
+  );
+  arrayOfAuthors = Object.keys(authorFreq).sort((a, b) => authorFreq[b] - authorFreq[a]);
+  let authorsSorted = [...new Set(arrayOfAuthors)];
+
+  return authorsSorted;
+}
