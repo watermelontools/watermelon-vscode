08db824926a00190dd5f3418d3cdccc39309025d
Esteban Dalel R <estebandalelr@gmail.com>
2022-04-30 21:40:13 -0500

    Create debounce function

---
+++ src/utils/debounce.ts
@@ -0,0 +1,16 @@
+export default function debounce(func: any, wait: number, immediate: any) {
+  let timeout: any;
+  return function () {
+    // @ts-ignore
+    let context = this;
+    let args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
