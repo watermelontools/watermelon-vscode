13a102741f0e645f593cbe148b454195dd81b873
estebandalelr <estebandalelr@gmail.com>
2022-05-11 13:20:54 -0500

    Make array unique

--- src/watermelonSidebar.ts
+++ src/watermelonSidebar.ts
@@ -146,35 +146,44 @@ export default class watermelonSidebar implements vscode.WebviewViewProvider {
           break;
         }
         case "blame": {
-/*           searchType({
+          /*           searchType({
             searchType: "webview.button",
             owner,
             repo,
             localUser,
             userEmail,
           }); */
-          let blamePromises =await getFullBlame(
-            
+          let blamePromises = await getFullBlame(
             vscode?.window?.activeTextEditor?.selection.start.line ?? 1,
-            vscode?.window?.activeTextEditor?.selection.end.line ??  vscode.window.activeTextEditor?.document.lineCount ?? 2,
+            vscode?.window?.activeTextEditor?.selection.end.line ??
+              vscode.window.activeTextEditor?.document.lineCount ??
+              2,
             vscode.window.activeTextEditor?.document.uri.fsPath,
             gitAPI
-            )
-            Promise.allSettled(blamePromises).then((results) => {
-              let blames: string[] = [];
-              results.forEach((result) => {
-                if (result.status === "fulfilled") {
-                  blames.push(result.value);
-                } else {
-                  blames.push(result.reason);
-                }
-              });
-              
-              this.sendMessage({
-                command: "blame",
-                data: blames,
-              });
+          );
+          Promise.allSettled(blamePromises).then((results) => {
+            let blames: string[] = [];
+            results.forEach((result) => {
+              if (result.status === "fulfilled") {
+                blames.push(result.value);
+              } else {
+                blames.push(result.reason);
+              }
             });
+
+            const uniqueBlames = [
+              ...new Map(
+                blames.map((item) =>
+                  // @ts-ignore
+                  [item["message"], item]
+                )
+              ).values(),
+            ];
+            this.sendMessage({
+              command: "blame",
+              data: uniqueBlames,
+            });
+          });
           break;
         }
       }
