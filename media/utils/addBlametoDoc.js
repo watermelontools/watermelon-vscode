255e5a6f37cf4efe11c3b4f39716caac4fe4ab77
estebandalelr <estebandalelr@gmail.com>
2022-05-11 13:09:36 -0500

    Create addBlametoDoc.js

---
+++ media/utils/addBlametoDoc.js
@@ -0,0 +1,45 @@
+import dateToHumanReadable from "./dateToHumanReadable.js";
+
+const addBlametoDoc = (blameArray) => {
+  console.log(blameArray);
+  $("#ghHolder").append(
+    "<button class='run-watermelon'>Run Watermelon</button><br/>"
+  );
+
+  $("#ghHolder").append(
+    `<button class='create-docs'>Create repo docs</button>`
+  );
+  $(".run-watermelon").on("click", (event) => {
+    sendMessage({ command: "run" });
+  });
+  $(".create-docs").on("click", (event) => {
+    sendMessage({ command: "create-docs" });
+  });
+  $("#ghHolder").append(`
+    <h3>Pull Requests</h3>
+    `);
+  $("#ghHolder").append(`
+    <table class="blame-table">
+      <thead>
+        <tr>
+          <th>Author</th>
+          <th>Message</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody class="blame-rows">
+      </tbody>
+    </table>
+    `);
+  blameArray.forEach((blameLine, index) => {
+    $(".blame-rows").append(`
+      <tr>
+        <td>${blameLine.authorName}</td>
+        <td>${blameLine.message}</td>
+        <td>${dateToHumanReadable(blameLine.commitDate)}</td>
+      </tr>
+      `);
+  });
+};
+
+export default addBlametoDoc;
