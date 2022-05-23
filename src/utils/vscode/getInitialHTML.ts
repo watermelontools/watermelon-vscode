a257c0da30dd758ebcd63922895a631ff298362f
Esteban Dalel R <estebandalelr@gmail.com>
2022-05-23 11:50:59 -0500

    Remove Beta tag from buttons

--- src/utils/vscode/getInitialHTML.ts
+++ src/utils/vscode/getInitialHTML.ts
@@ -22,7 +22,7 @@ export default function getInitialHTML(
     "https://*.github.io",
     "https://cdn.webflow.com/",
     "https://codecov.io/",
-    "https://i.imgur.com"
+    "https://i.imgur.com",
   ];
   let scriptSources = [
     `'nonce-${nonce}'`,
@@ -90,7 +90,7 @@ export default function getInitialHTML(
            <p>We will fetch the associated PRs and comments for you to understand the context of the code</p>
            <button class='git-blame'>View Git Blame</button>
            <p>Click this button to enrich your code with relevant information from GitHub:</p>
-           <button class='run-watermelon'>View Pull Requests (Beta)</button>
+           <button class='run-watermelon'>View Pull Requests</button>
            <p>Alternatively, you can <a href="https://github.com/watermelontools/wm-extension#commands">run with our watermelon.start command</a></p>
         </div>
         <h2>Need help?</h2>
