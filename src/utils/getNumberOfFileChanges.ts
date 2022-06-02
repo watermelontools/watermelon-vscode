753be41cdee5d11eab21971fa82063daee73c19c
baristaGeek <estebanvargas94@gmail.com>
2022-06-02 10:24:43 -0500

    get number of file changes

--- src/utils/getNumberOfFileChanges.ts
+++ src/utils/getNumberOfFileChanges.ts
@@ -1,20 +1,8 @@
-import * as vscode from "vscode";
-import getFullBlame from "./getFullBlame";
-
 async function getNumberOfFileChanges(gitAPI:any, filePath:string){
-    let blameArray: string[] = [];
     let blames = await gitAPI?.repositories[0].blame(
         filePath || "."
     );
-
-    blames.split("\n").forEach((line: String) => {
-        blameArray.push(line.split(" ")[0]);
-    });
-
-    let uniqueBlames = [
-        ...new Set(blameArray),
-    ];
-   
-    return uniqueBlames.length;
+    const numberOfLinesInBlame = blames.length;
+    return numberOfLinesInBlame;
 }
 export default getNumberOfFileChanges;
