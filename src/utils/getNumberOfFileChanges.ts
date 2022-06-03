5dfc3b04bfa4dbc2a09465e259d169285b5ac38a
baristaGeek <estebanvargas94@gmail.com>
2022-06-03 14:01:40 -0500

    Revert to counting unique commit hashes

--- src/utils/getNumberOfFileChanges.ts
+++ src/utils/getNumberOfFileChanges.ts
@@ -2,7 +2,13 @@ async function getNumberOfFileChanges(gitAPI:any, filePath:string){
     let blames = await gitAPI?.repositories[0].blame(
         filePath || "."
     );
-    const numberOfLinesInBlame = blames.length;
-    return numberOfLinesInBlame;
+    let commitHashArray = [] as String[];
+    let splitBlames = blames.split("\n").map((line:string) => {
+        commitHashArray.push(line.split(" ")[0]);
+    });
+
+    // get number of unique entries in commitHashArray
+    let uniqueCommitHashArray = [...new Set(commitHashArray)];
+    return uniqueCommitHashArray.length;
 }
 export default getNumberOfFileChanges;
