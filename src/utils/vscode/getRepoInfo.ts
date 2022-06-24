aa1b407f867a3e6d32b732e9892283da85cb855f
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-24 17:08:56 -0400

    Use git-url-parse to get repo info

--- src/utils/vscode/getRepoInfo.ts
+++ src/utils/vscode/getRepoInfo.ts
@@ -1,32 +1,23 @@
 import getGitAPI from "./getGitAPI";
 import { nonGHRepo } from "./showErrors";
+import * as gitURLparse from "git-url-parse";
 
 export default async function getRepoInfo(): Promise<{
-  ownerUsername: string;
-  repoName: string;
+  owner: string;
+  repo: string;
 }> {
   let gitAPI = await getGitAPI();
-  let ownerUsername: string = "";
-  let repoName: string = "";
+  let owner: string = "";
+  let repo: string = "";
 
   let config = await await gitAPI?.repositories[0]?.getConfig(
     "remote.origin.url"
   );
-  if (config?.includes("https://")) {
-    if (config?.includes("github.com")) {
-      repoName = config?.split("/")[4].split(".")[0];
-      ownerUsername = config?.split("/")[3];
-      return { ownerUsername, repoName };
-    } else {
-      nonGHRepo();
-      return {
-        ownerUsername: "watermelon",
-        repoName: "wm-extension"
-      };
-    }
-  } else {
-    repoName = config?.split("/")[1].split(".")[0] ?? "";
-    ownerUsername = config?.split(":")[1].split("/")[0] ?? "";
+  if (config) {
+    let parsed = gitURLparse(config);
+    owner = parsed.owner;
+    repo = parsed.name;
   }
-  return { ownerUsername, repoName };
+
+  return { owner, repo };
 }
