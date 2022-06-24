7d0e33964354e26369ee0e3b9e7357984a226522
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-24 17:12:26 -0400

    handle non github repos

--- src/utils/vscode/getRepoInfo.ts
+++ src/utils/vscode/getRepoInfo.ts
@@ -5,7 +5,7 @@ import * as gitURLparse from "git-url-parse";
 export default async function getRepoInfo(): Promise<{
   owner: string;
   repo: string;
-}> {
+} | void> {
   let gitAPI = await getGitAPI();
   let owner: string = "";
   let repo: string = "";
@@ -17,6 +17,9 @@ export default async function getRepoInfo(): Promise<{
     let parsed = gitURLparse(config);
     owner = parsed.owner;
     repo = parsed.name;
+    if (!(parsed.source.includes("github"))) {
+      nonGHRepo();
+    }
   }
 
   return { owner, repo };
