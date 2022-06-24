99816e46895b5e2626cceddc06cfc8a64ed9e5cb
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-24 17:20:00 -0400

    New VSCode tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,31 @@
 import * as assert from "assert";
 
-// You can import and use all API from the 'vscode' module
+// you can import and use all API from the "vscode" module
 // as well as import your extension to test it
 import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
+// import * as myExtension from '../src/extension';
 
-suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
 
-  test("Sample test", () => {
-    assert.strictEqual([1, 2, 3].indexOf(5), -1);
-    assert.strictEqual([1, 2, 3].indexOf(0), -1);
+suite('Watermelon extension tests', () => {
+  test('Extension should be active after startup', (done) => {
+    setTimeout(() => {
+      const extension = vscode.extensions.getExtension(
+        'WatermelonTools.watermelon-tools'
+      );
+      assert.ok(extension);
+      assert.equal(extension.isActive, true);
+      done();
+    }, 1000 * 3);
+  }).timeout(1000 * 10);
+
+  test('should register commands', (done) => {
+    vscode.commands
+      .getCommands(true)
+      .then((commands) =>{
+       let wmCommands= commands.filter((command) => command.startsWith('watermelon'));
+       console.log(wmCommands);
+       assert.equal(commands.length > 0, true);
+      })
+      .then(() => done());
   });
 });
