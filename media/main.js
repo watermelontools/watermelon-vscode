b72404def23d4638e0c50df6432957561128f096
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-27 13:48:28 -0400

    Extract Daily summary

--- media/main.js
+++ media/main.js
@@ -11,6 +11,7 @@ import addBlametoDoc from "./utils/addBlametoDoc.js";
 import addGHUserInfo from "./utils/addGHUserInfo.js";
 import addVersionToFooter from "./utils/addVersionToFooter.js";
 import addSessionToFooter from "./utils/addSessionToFooter.js";
+import addDailySummary from "./utils/addDailySummary.js";
 
 let errorTimeout;
 
@@ -45,81 +46,7 @@ function handleMessage(message) {
       addGHUserInfo(message.data);
       break;
     case "dailySummary":
-      $("#dailySummary").append(`
-      <div id="assignedIssues">
-      <h3>Issues Assigned to You in this Repo</h3>
-      </div>
-      <div id="creatorIssues">
-      <h3>Issues You Created in this Repo</h3>
-      </div>
-      <div id="mentionedIssues">
-      <h3>Issues that Mentioned You in this Repo</h3>
-      </div>
-        <div id="globalIssues">
-        <h3>Open Issues Assigned to You in All of GitHub</h3>
-        </div>
-      `);
-      if (message.data.globalIssues.length > 0) {
-        message.data.globalIssues.map((issue) => {
-          $("#globalIssues").append(`
-        <div>
-        <a href="${issue.html_url}">${issue.title}</a>
-        </div>
-        `);
-        });
-      } else {
-        $("#globalIssues").append(`
-      <div>
-      <p>You have no open issues assigned to you</p>
-      </div>
-      `);
-      }
-      if (message.data.assignedIssues.length > 0) {
-        message.data.assignedIssues.map((issue) => {
-          $("#assignedIssues").append(`
-  <div>
-  <a href="${issue.html_url}">${issue.title}</a>
-  </div>
-  `);
-        });
-      } else {
-        $("#assignedIssues").append(`
-<div>
-<p>You have no open issues assigned to you 🧘</p>
-</div>
-`);
-      }
-      if (message.data.creatorIssues.length > 0) {
-        message.data.creatorIssues.map((issue) => {
-          $("#creatorIssues").append(`
-  <div>
-  <a href="${issue.html_url}">${issue.title}</a>
-  </div>
-  `);
-        });
-      } else {
-        $("#creatorIssues").append(`
-<div>
-<p>You have no open issues created 🌵</p>
-</div>
-`);
-      }
-      if (message.data.mentionedIssues.length > 0) {
-        message.data.mentionedIssues.map((issue) => {
-          $("#mentionedIssues").append(`
-  <div>
-  <a href="${issue.html_url}">${issue.title}</a>
-  </div>
-  `);
-        });
-      } else {
-        $("#mentionedIssues").append(`
-<div>
-<p>You have no open issues that mention you 🙊</p>
-</div>
-`);
-      }
-
+      addDailySummary(message.data);
       break;
     case "prs":
       removeLoading(errorTimeout);
