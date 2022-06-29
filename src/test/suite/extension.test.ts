5079ffaa6408b47616f20d3317ccc716705381da
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-29 12:48:44 -0400

    Added command list test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,28 +3,68 @@ import * as assert from "assert";
 // you can import and use all API from the "vscode" module
 // as well as import your extension to test it
 import * as vscode from "vscode";
+import {
+  WATERMELON_HISTORY_COMMAND,
+  WATERMELON_MULTI_SELECT_COMMAND,
+  WATERMELON_PULLS_COMMAND,
+  WATERMELON_SELECT_COMMAND,
+  WATERMELON_SHOW_COMMAND,
+} from "../../constants";
 // import * as myExtension from '../src/extension';
 
-
-suite('Watermelon extension tests', () => {
-  test('Extension should be active after startup', (done) => {
+suite("Watermelon extension tests", () => {
+  test("Extension should be active after startup", (done) => {
     setTimeout(() => {
       const extension = vscode.extensions.getExtension(
-        'WatermelonTools.watermelon-tools'
+        "WatermelonTools.watermelon-tools"
       );
       assert.ok(extension);
-      assert.equal(extension.isActive, true);
+      assert.strictEqual(extension.isActive, true);
       done();
     }, 1000 * 3);
   }).timeout(1000 * 10);
 
-  test('should register commands', (done) => {
+  test("should register commands", (done) => {
     vscode.commands
       .getCommands(true)
-      .then((commands) =>{
-       let wmCommands= commands.filter((command) => command.startsWith('watermelon'));
-       console.log(wmCommands);
-       assert.equal(commands.length > 0, true);
+      .then((commands) => {
+        let wmCommands = commands.filter((command) =>
+          command.startsWith("watermelon")
+        );
+        console.log(wmCommands);
+        assert.strictEqual(commands.length > 0, true);
+      })
+      .then(() => done());
+  });
+
+  test("should show extension when show command is called", (done) => {
+    vscode.commands
+      .executeCommand(WATERMELON_SHOW_COMMAND)
+      .then(() => {
+        vscode.window.showInformationMessage("Watermelon is active!");
+      })
+      .then(() => done());
+  });
+
+  test("should contain all  commands", (done) => {
+    vscode.commands
+      .getCommands(true)
+      .then((commands) => {
+        let wmCommands = commands.filter((command) =>
+          command.startsWith("watermelon")
+        );
+        let commandList = [
+          WATERMELON_SHOW_COMMAND,
+          WATERMELON_SELECT_COMMAND,
+          WATERMELON_MULTI_SELECT_COMMAND,
+          WATERMELON_PULLS_COMMAND,
+          WATERMELON_HISTORY_COMMAND,
+        ];
+
+        assert.strictEqual(
+          commandList.every((command) => wmCommands.includes(command)),
+          true
+        );
       })
       .then(() => done());
   });
