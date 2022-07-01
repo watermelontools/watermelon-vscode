e607dd207737c5158f380e426460b32654d62612
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-01 16:33:26 -0400

    Increase timeout to avoid blink

--- media/utils/setLoading.js
+++ media/utils/setLoading.js
@@ -6,7 +6,7 @@ function setLoading(errorTimeout) {
     <p> <i class='codicon codicon-loading loader-spinner'></i>Loading</p>
     </div>
     `);
-  return (errorTimeout = setTimeout(setError, 4000));
+  return (errorTimeout = setTimeout(setError, 5000));
 }
 function setError() {
   $("#ghHolder").replaceWith(`
