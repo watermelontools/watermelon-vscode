8b11d164c5368cf73c6b47ed1292aed9deab737a
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-01 10:04:51 -0400

    Use Primer on buttons

--- media/utils/addActionButtons.js
+++ media/utils/addActionButtons.js
@@ -4,12 +4,12 @@ const addActionButtons = () => {
   $("#ghHolder").append(`<p>Higlight a piece of code to start.</p>`);
   $("#ghHolder").append(
     `<p>We will fetch the commit history for you to understand the context of the code</p>
-        <button class='git-blame'>View Commit History</button>
+        <button class="git-blame btn btn-primary" type="button">View Commit History</button>
         <br/>`
   );
   $("#ghHolder").append(
     `<p>Click this button to enrich your code with relevant information from GitHub:</p>
-    <button class='run-watermelon'>View Pull Requests</button><br/>`
+    <button class="run-watermelon btn btn-primary" type="button">View Pull Requests</button><br/>`
   );
 
   $(".run-watermelon").on("click", (event) => {
