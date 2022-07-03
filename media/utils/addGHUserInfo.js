035584f5fdbed1d298b9b50818e7c818b32e6d2d
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-03 11:53:06 -0500

    Add login info to dropdown

--- media/utils/addGHUserInfo.js
+++ media/utils/addGHUserInfo.js
@@ -2,5 +2,11 @@ const addGHUserInfo = ({ login, avatar }) => {
     $('.replace-user-img').replaceWith(`
         <img src="${avatar}" class="login-image"/>
     `);
+    console.log(login)
+    $(".login-info").append(`
+        <p class="login-info-text">
+            Logged in as ${login}
+        </p>
+    `);
 };
 export default addGHUserInfo;
