aea5038613f63946d7c9ff0e241c9b546fcd9377
estebandalelr <estebandalelr@gmail.com>
2022-07-05 11:35:56 -0500

    Fix function to read status codes

--- src/utils/github/checkIfUserStarred.ts
+++ src/utils/github/checkIfUserStarred.ts
@@ -1,10 +1,27 @@
-export default async function checkIfUserStarred({ octokit }: { octokit: any }) {
-    let octoresp = await octokit.rest.activity.checkRepoIsStarredByAuthenticatedUser(
-      {
-        owner: 'watermelontools',
-        repo: 'wm-extension'
-      });
-      console.log("isstared func ",octoresp.error);
-    
-  }
-  
+import debugLogger from "../vscode/debugLogger";
+
+export default async function checkIfUserStarred({
+  octokit,
+}: {
+  octokit: any;
+}) {
+  // @ts-ignore
+  let isStarred = await octokit.rest.activity
+    .checkRepoIsStarredByAuthenticatedUser({
+      owner: "watermelontools",
+      repo: "wm-extension",
+    })
+    .then((res: any) => {
+      debugLogger(res.status);
+      if (res.status === 204) {
+        return true;
+      }
+    })
+    .catch((err: any) => {
+      debugLogger(err.status);
+      if (err.status === 404) {
+        return false;
+      }
+    });
+  return await isStarred;
+}
