cc9695dec23c62bd07caed50bd291bcf62cf6643
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-07 13:31:44 -0500

    Add test over local folder

--- src/test/suite/git.test.ts
+++ src/test/suite/git.test.ts
@@ -2,6 +2,13 @@ import * as assert from "assert";
 import getRepoInfo from "../../utils/vscode/getRepoInfo";
 
 suite("Git tests", () => {
+    test("should parse from local files", async () => {
+        let { owner, repo, source, protocol } = await getRepoInfo({});
+        assert.strictEqual(owner, "watermelontools");
+        assert.strictEqual(repo, "wm-extension");
+        assert.strictEqual(source, "github.com");
+        assert.strictEqual(protocol, "https");
+    });
     const testCases = [
         { protocol: "http", url: "http://github.com/watermelontools/wm-extension.git" },
         { protocol: "http", url: "http://estebandalelr@github.com/watermelontools/wm-extension.git" },
@@ -16,7 +23,7 @@ suite("Git tests", () => {
     ];
     for (let index = 0; index < testCases.length; index++) {
         const element = testCases[index];
-        test(`should parse user, repo, source and protocol from "${element.url}"`,async () => {
+        test(`should parse user, repo, source and protocol from "${element.url}"`, async () => {
             let { owner, repo, source, protocol } = await getRepoInfo({ repoURL: element.url });
             assert.strictEqual(owner, "watermelontools");
             assert.strictEqual(repo, "wm-extension");
