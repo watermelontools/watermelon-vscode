9f72dd2af6e33f25612bf905ccd6d9087a76b848
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-12 12:21:58 -0500

    Add comment indicator

--- media/utils/addDailySummary.js
+++ media/utils/addDailySummary.js
@@ -45,16 +45,29 @@ const addDailySummary = (data) => {
       `);
       globalIssuesPerRepo[repoName].forEach((issue) => {
         $(`#${repoName}`).append(`
-
-      <div class="Box-row">
-      <a href="${issue.html_url}">${issue.title}</a>
-      <p class="text-small">
-        By ${
-          issue.user.login
-        } on <span class="text-light">${dateToHumanReadable(
+      <div class="Box-row d-flex flex-justify-between">
+        <div class="Box-row-cell">
+          <a href="${issue.html_url}">${issue.title}</a>
+          <p class="text-small">
+            By ${
+              issue.user.login
+            } on <span class="text-light">${dateToHumanReadable(
           issue.created_at
         )}</span>
-      </p>
+          </p>
+        </div>
+        ${
+          issue.comments
+            ? `
+            <div class="Box-row-cell d-flex flex-justify-center flex-items-center">
+              <div class="icon-holder">
+                <i class="codicon codicon-comment"></i>
+              </div>
+              <span>${issue.comments}</span>
+            </div>
+          `
+            : ""
+        }
       </div>
       `);
       });
