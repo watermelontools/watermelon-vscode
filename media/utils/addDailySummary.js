c11329524ff6ea87d48422fd83dfd8490164653c
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-12 17:38:43 -0500

    Test box function for all issues

--- media/utils/addDailySummary.js
+++ media/utils/addDailySummary.js
@@ -1,9 +1,38 @@
 import dateToHumanReadable from "./dateToHumanReadable.js";
+const issueBox = (issue) => {
+  return `
+  <div class="Box-row Box-row--hover-gray d-flex flex-justify-between">
+      <div class="Box-row-cell">
+        <a href="${issue.html_url}">${issue.title}</a>
+        <p class="text-small">
+          By ${
+            issue.user.login
+          } on <span class="text-light">${dateToHumanReadable(
+    issue.created_at
+  )}</span>
+        </p>
+      </div>
+      ${
+        issue.comments
+          ? `
+          <div class="Box-row-cell d-flex flex-justify-center flex-items-center">
+            <div class="icon-holder">
+              <i class="codicon codicon-comment"></i>
+            </div>
+            <span>${issue.comments}</span>
+          </div>
+        `
+          : ""
+      }
+    </div>
+  </div>
+  `;
+};
+
 const addDailySummary = (data) => {
   $("#dailySummary").empty();
   $("#dailySummary").append(`
     <div id="assignedIssues">
-    <h3>Issues Assigned to You in this Repo</h3>
     </div>
     <div id="creatorIssues">
     <h3>Issues You Created in this Repo</h3>
@@ -80,12 +109,18 @@ const addDailySummary = (data) => {
     `);
   }
   if (data.assignedIssues.length > 0) {
+    $("#assignedIssues").append(`
+    <div class="Box anim-fade-in">
+      <div class="Box-header">
+        <h3 class="Box-title">Issues Assigned to You in this Repo</h3>
+      </div>
+    </div>
+    `);
     data.assignedIssues.map((issue) => {
-      $("#assignedIssues").append(`
-<div>
-<a href="${issue.html_url}">${issue.title}</a>
-</div>
-`);
+      $("#assignedIssues .Box").append(`
+      ${issueBox(issue)}
+
+      `);
     });
   } else {
     $("#assignedIssues").append(`
