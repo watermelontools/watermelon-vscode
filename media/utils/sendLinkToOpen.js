002c60f2afadcd928ddc6cd4e72b408fe89e6241
estebandalelr <estebandalelr@gmail.com>
2022-07-13 17:13:30 -0500

    Add handler for links with analytics

---
+++ media/utils/sendLinkToOpen.js
@@ -0,0 +1,8 @@
+import sendMessage from "./sendVSCodeMessage.js";
+
+export default function sendLinkToOpen({ link, source }) {
+  sendMessage({
+    command: "link",
+    data: { link, source },
+  });
+}
