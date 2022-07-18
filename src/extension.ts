a1ac7207afe5cba8cbc6d9176365e951877dce91
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-18 07:12:11 -0500

    Added CodeLens handler

--- src/extension.ts
+++ src/extension.ts
@@ -217,8 +217,45 @@ export async function activate(context: vscode.ExtensionContext) {
       });
     }
   };
+  async function addConsoleLog() {
+    let lineNumStr = await vscode.window.showInputBox({
+      prompt: "Line Number",
+    });
 
+    let lineNum = lineNumStr ? parseInt(lineNumStr) : 0;
+    let insertionLocation = new vscode.Range(lineNum - 1, 0, lineNum - 1, 0);
+    let snippet = new vscode.SnippetString("console.log($1);\n");
+
+    vscode.window.activeTextEditor?.insertSnippet(snippet, insertionLocation);
+  }
+  class MyCodeLensProvider implements vscode.CodeLensProvider {
+    async provideCodeLenses(
+      document: vscode.TextDocument
+    ): Promise<vscode.CodeLens[]> {
+      let topOfDocument = new vscode.Range(0, 0, 0, 0);
+
+      let c: vscode.Command = {
+        command: "watermelon.addConsoleLog",
+        title: "Insert console.log",
+      };
+
+      let codeLens = new vscode.CodeLens(topOfDocument, c);
+
+      return [codeLens];
+    }
+  }
+  let docSelector = {
+    language: "javascript",
+    scheme: "file",
+  };
+  let codeLensProviderDisposable = vscode.languages.registerCodeLensProvider(
+    docSelector,
+    new MyCodeLensProvider()
+  );
+
+  context.subscriptions.push(codeLensProviderDisposable);
   context.subscriptions.push(
+    vscode.commands.registerCommand("watermelon.addConsoleLog", addConsoleLog),
     vscode.commands.registerCommand(
       WATERMELON_SHOW_COMMAND,
       showCommandHandler
