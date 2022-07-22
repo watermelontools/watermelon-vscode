741458958c66ffd659dba97e724dad221ddd7144
baristaGeek <estebanvargas94@gmail.com>
2022-07-22 18:37:39 -0500

    Replace hyperlinks with a tags that contain the hyperlink

---
+++ media/utils/replaceHyperlinks.js
@@ -0,0 +1,9 @@
+const replaceHyperlinks = (text) => {
+    let hyperlink = text.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm)[0];
+    return text
+      .replaceAll(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm,
+        `<a href=${hyperlink}>${hyperlink}</a>`
+      );
+  };
+  export default replaceHyperlinks;
