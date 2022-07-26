37bfca826882e71b07377e934d3d944e0eceef6a
baristaGeek <estebanvargas94@gmail.com>
2022-07-26 18:27:19 -0500

    Add skeleton of blame table

--- media/utils/addContextToDoc.js
+++ media/utils/addContextToDoc.js
@@ -4,58 +4,28 @@ import dateToHumanReadable from "./dateToHumanReadable.js";
 import parseComments from "./parseComments.js";
 import addActionButtons from "./addActionButtons.js";
 const addContextToDoc = (prs, commitLink) => {
-
-
-    // Add blame to doc
-
-    // addActionButtons();
-    // $("#ghHolder").append(`
-    //   <h3>Commits</h3>
-    //   `);
-    // $("#ghHolder").append(`
-    //   <table class="Box anim-fade-in">
-    //     <thead class="Box-header">
-    //       <tr>
-    //         <th>Commit</th>
-    //         <th>Author</th>
-    //         <th>Message</th>
-    //         <th>Date</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody class="blame-rows">
-    //     </tbody>
-    //   </table>
-    //   `);
-    // //sort array by date, newest first
-    // blameArray.sort((a, b) => {
-    //   return new Date(b.commitDate) - new Date(a.commitDate);
-    // });
-    // blameArray.forEach((blameLine, index) => {
-    //   $(".blame-rows").append(`
-    //     <tr ${index % 2 === 1 ? 'class="Box-row--gray"' : ""}>
-    //       <td>
-    //       ${
-    //         commitLink
-    //           ? `<a href='${commitLink}${blameLine?.hash}'>
-    //             ${blameLine?.hash?.slice(0, 7)}
-    //             </a>`
-    //           : blameLine?.hash?.slice(0, 7)
-    //       }
-    //       </td>
-    //       <td>${blameLine.authorName}</td>
-    //       <td>${(blameLine.message)}</td>
-    //       <td>${dateToHumanReadable(blameLine.commitDate)}</td>
-    //     </tr>
-    //     `);
-    // });
-
-    // Add PRs to Doc
-
-    console.log("message, commitlink", prs, commitLink);
+  console.log("message, commitlink", prs, commitLink);
+  
   addActionButtons();
   $("#ghHolder").append(`
   <h3>Code Context</h3>
   `);
+
+    // Latest commit message
+    $("#ghHolder").append(`
+    <table class="Box anim-fade-in">
+      <thead class="Box-header">
+        <tr>
+          <th>Commit</th>
+          <th>Author</th>
+          <th>Message</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody class="blame-rows">
+      </tbody>
+    </table>
+    `);
 
     // Most Relevant PR
     $("#ghHolder").append(`
