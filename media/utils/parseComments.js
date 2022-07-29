0a980356f188dddfffcd269a6558bd44cdd33213
baristaGeek <estebanvargas94@gmail.com>
2022-07-29 11:53:13 -0500

    Remove optional chaining for parsing comments

--- media/utils/parseComments.js
+++ media/utils/parseComments.js
@@ -19,14 +19,14 @@ function parseComments(comment) {
         <div class="Box">
         <div class="Box-header">
           <h5 class="comment-author" title="View this user on github" class='pr-author-combo'>
-          <a href="${comment?.user.html_url}" >  
-          <img src="${comment?.user.avatar_url}" class='pr-author-img'/>
-            ${comment?.user.login}
+          <a href="${comment.user.html_url}" >  
+          <img src="${comment.user.avatar_url}" class='pr-author-img'/>
+            ${comment.user.login}
           </a>  <span class="pr-date"> on ${dateToHumanReadable(comment?.created_at)}</span>
           </h5>
         </div>
         <div class="Box-body markdown-body">
-      ${comment?.body ? replaceUserTags(marked.parse(comment.body, { gfm: true, breaks: true })) : ""}
+      ${comment.body ? replaceUserTags(marked.parse(comment.body, { gfm: true, breaks: true })) : ""}
         </div>
         </div>`;
   return mdComments;
