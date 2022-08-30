79a9921276517a660a7bd96cd6b5721bdf1bb264
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-30 14:16:49 -0700

    Test auth

--- src/auth.ts
+++ src/auth.ts
@@ -4,19 +4,41 @@ import {
   AuthenticationProviderAuthenticationSessionsChangeEvent,
   AuthenticationSession,
   Disposable,
+  env,
   EventEmitter,
   ExtensionContext,
+  ProgressLocation,
+  Uri,
+  UriHandler,
+  window,
 } from "vscode";
-
+import { PromiseAdapter, promiseFromEvent } from "./utils/authUtils";
+import { randomUUID } from "crypto";
+import axios from "axios";
 export const AUTH_TYPE = `watermelon`;
 const AUTH_NAME = `watermelon`;
+const CLIENT_ID = `3GUryQ7ldAeKEuD2obYnppsnmj58eP5u`;
+const AUTH0_DOMAIN = `dev-txghew0y.us.auth0.com`;
+const SESSIONS_SECRET_KEY = `${AUTH_TYPE}.sessions`;
 
-export class WatermelonAuthenticationProvider
+class UriEventHandler extends EventEmitter<Uri> implements UriHandler {
+  public handleUri(uri: Uri) {
+    this.fire(uri);
+  }
+}
+
+export class Auth0AuthenticationProvider
   implements AuthenticationProvider, Disposable
 {
   private _sessionChangeEmitter =
     new EventEmitter<AuthenticationProviderAuthenticationSessionsChangeEvent>();
   private _disposable: Disposable;
+  private _pendingStates: string[] = [];
+  private _codeExchangePromises = new Map<
+    string,
+    { promise: Promise<string>; cancel: EventEmitter<void> }
+  >();
+  private _uriHandler = new UriEventHandler();
 
   constructor(private readonly context: ExtensionContext) {
     this._disposable = Disposable.from(
@@ -25,12 +47,19 @@ export class WatermelonAuthenticationProvider
         AUTH_NAME,
         this,
         { supportsMultipleAccounts: false }
-      )
+      ),
+      window.registerUriHandler(this._uriHandler)
     );
   }
 
   get onDidChangeSessions() {
     return this._sessionChangeEmitter.event;
+  }
+
+  get redirectUri() {
+    const publisher = this.context.extension.packageJSON.publisher;
+    const name = this.context.extension.packageJSON.name;
+    return `${env.uriScheme}://${publisher}.${name}`;
   }
 
   /**
@@ -41,6 +70,12 @@ export class WatermelonAuthenticationProvider
   public async getSessions(
     scopes?: string[]
   ): Promise<readonly AuthenticationSession[]> {
+    const allSessions = await this.context.secrets.get(SESSIONS_SECRET_KEY);
+
+    if (allSessions) {
+      return JSON.parse(allSessions) as AuthenticationSession[];
+    }
+
     return [];
   }
 
@@ -50,19 +85,192 @@ export class WatermelonAuthenticationProvider
    * @returns
    */
   public async createSession(scopes: string[]): Promise<AuthenticationSession> {
-    return null as any as AuthenticationSession;
+    try {
+      const token = await this.login(scopes);
+      if (!token) {
+        throw new Error(`Auth0 login failure`);
+      }
+
+      const userinfo: { name: string; email: string; id: string } =
+        await this.getUserInfo(token);
+
+      const session: AuthenticationSession = {
+        id: userinfo.id,
+        accessToken: token,
+        account: {
+          label: userinfo.name,
+          id: userinfo.email,
+        },
+        scopes: [],
+      };
+
+      await this.context.secrets.store(
+        SESSIONS_SECRET_KEY,
+        JSON.stringify([session])
+      );
+
+      this._sessionChangeEmitter.fire({
+        added: [session],
+        removed: [],
+        changed: [],
+      });
+
+      return session;
+    } catch (e) {
+      window.showErrorMessage(`Sign in failed: ${e}`);
+      throw e;
+    }
   }
 
   /**
    * Remove an existing session
    * @param sessionId
    */
-  public async removeSession(sessionId: string): Promise<void> {}
+  public async removeSession(sessionId: string): Promise<void> {
+    const allSessions = await this.context.secrets.get(SESSIONS_SECRET_KEY);
+    if (allSessions) {
+      let sessions = JSON.parse(allSessions) as AuthenticationSession[];
+      const sessionIdx = sessions.findIndex((s) => s.id === sessionId);
+      const session = sessions[sessionIdx];
+      sessions.splice(sessionIdx, 1);
+
+      await this.context.secrets.store(
+        SESSIONS_SECRET_KEY,
+        JSON.stringify(sessions)
+      );
+
+      if (session) {
+        this._sessionChangeEmitter.fire({
+          added: [],
+          removed: [session],
+          changed: [],
+        });
+      }
+    }
+  }
 
   /**
    * Dispose the registered services
    */
   public async dispose() {
     this._disposable.dispose();
+  }
+
+  /**
+   * Log in to Auth0
+   */
+  private async login(scopes: string[] = []) {
+    return await window.withProgress<string>(
+      {
+        location: ProgressLocation.Notification,
+        title: "Signing in to Auth0...",
+        cancellable: true,
+      },
+      async (_, token) => {
+        const stateId = randomUUID();
+
+        this._pendingStates.push(stateId);
+
+        const scopeString = scopes.join(" ");
+
+        if (!scopes.includes("openid")) {
+          scopes.push("openid");
+        }
+        if (!scopes.includes("profile")) {
+          scopes.push("profile");
+        }
+        if (!scopes.includes("email")) {
+          scopes.push("email");
+        }
+
+        const searchParams = new URLSearchParams([
+          ["response_type", "token"],
+          ["client_id", CLIENT_ID],
+          ["redirect_uri", this.redirectUri],
+          ["state", stateId],
+          ["scope", scopes.join(" ")],
+          ["prompt", "login"],
+        ]);
+        const uri = Uri.parse(
+          `https://${AUTH0_DOMAIN}/authorize?${searchParams.toString()}`
+        );
+        await env.openExternal(uri);
+
+        let codeExchangePromise = this._codeExchangePromises.get(scopeString);
+        if (!codeExchangePromise) {
+          codeExchangePromise = promiseFromEvent(
+            this._uriHandler.event,
+            this.handleUri(scopes)
+          );
+          this._codeExchangePromises.set(scopeString, codeExchangePromise);
+        }
+
+        try {
+          return await Promise.race([
+            codeExchangePromise.promise,
+            new Promise<string>((_, reject) =>
+              setTimeout(() => reject("Cancelled"), 60000)
+            ),
+            promiseFromEvent<any, any>(
+              token.onCancellationRequested,
+              (_, __, reject) => {
+                reject("User Cancelled");
+              }
+            ).promise,
+          ]);
+        } finally {
+          this._pendingStates = this._pendingStates.filter(
+            (n) => n !== stateId
+          );
+          codeExchangePromise?.cancel.fire();
+          this._codeExchangePromises.delete(scopeString);
+        }
+      }
+    );
+  }
+
+  /**
+   * Handle the redirect to VS Code (after sign in from Auth0)
+   * @param scopes
+   * @returns
+   */
+  private handleUri: (
+    scopes: readonly string[]
+  ) => PromiseAdapter<Uri, string> =
+    (scopes) => async (uri, resolve, reject) => {
+      const query = new URLSearchParams(uri.fragment);
+      const access_token = query.get("access_token");
+      const state = query.get("state");
+
+      if (!access_token) {
+        reject(new Error("No token"));
+        return;
+      }
+      if (!state) {
+        reject(new Error("No state"));
+        return;
+      }
+
+      // Check if it is a valid auth request started by the extension
+      if (!this._pendingStates.some((n) => n === state)) {
+        reject(new Error("State not found"));
+        return;
+      }
+
+      resolve(access_token);
+    };
+
+  /**
+   * Get the user info from Auth0
+   * @param token
+   * @returns
+   */
+  private async getUserInfo(token: string): Promise<any> {
+    const response = await axios.get(`https://${AUTH0_DOMAIN}/userinfo`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return await response;
   }
 }
