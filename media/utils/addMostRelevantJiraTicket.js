72b561a87e63dacef9cefa2ae21865106ba03487
baristaGeek <estebanvargas94@gmail.com>
2022-09-26 10:32:44 -0500

    Removed repeated title on html

--- media/utils/addMostRelevantJiraTicket.js
+++ media/utils/addMostRelevantJiraTicket.js
@@ -1,5 +1,4 @@
 const addMostRelevantJiraTicket = (jiraTicket) => {
-  $("#mostRelevantJiraTicketHolder").append(`<h3>Most Relevant Jira Ticket</h3>`);
   $("#mostRelevantJiraTicketHolder").append(`
   <h3>Assigned Jira Tickets</h3>
     <div class="Box-header">
