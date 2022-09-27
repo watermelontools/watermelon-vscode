a7a9c37f5cf3de4a234f243035268538e5ae5f0b
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-27 16:48:14 -0500

    Send user as user instead of userEmail

--- src/utils/jira/getAssignedJiraTickets.ts
+++ src/utils/jira/getAssignedJiraTickets.ts
@@ -3,20 +3,20 @@ import { backendURL } from "../../constants";
 import analyticsReporter from "../../utils/vscode/reporter";
 
 export default async function getAssignedJiraTickets({
-  userEmail,
+  user,
 }: {
-  userEmail: string;
+  user: string;
 }) {
-  const assignedJiraTickets = await axios.post(
-    `${backendURL}/api/jira/getAssignedTicketsInProgress`,
-    {
-      userEmail,
-    }
-  ).then(res => res.data).catch(err => {
-    let reporter = analyticsReporter();
-    let { message } = err;
-    reporter?.sendTelemetryException(err, { 'error': message });
-  });
+  const assignedJiraTickets = await axios
+    .post(`${backendURL}/api/jira/getAssignedTicketsInProgress`, {
+      user,
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      let reporter = analyticsReporter();
+      let { message } = err;
+      reporter?.sendTelemetryException(err, { error: message });
+    });
 
   return assignedJiraTickets;
 }
