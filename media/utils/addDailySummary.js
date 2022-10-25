0d69222d942201005ad4a9d62dda0b533bd42f53
Esteban Dalel R <estebandalelr@gmail.com>
2022-10-25 12:11:17 -0500

    Add jira prompt to daily summary

--- media/utils/addDailySummary.js
+++ media/utils/addDailySummary.js
@@ -6,6 +6,11 @@ const addDailySummary = ({ gitHubIssues, jiraTickets }) => {
   $("#dailySummary").empty();
   if (jiraTickets) {
     addJiraTicketsToDailySummary(jiraTickets);
+  } else {
+    $("#dailySummary").append(`<h3>Assigned Jira Tickets</h3>`);
+    $("#dailySummary").append(
+      `<p>Jira is a premium feature, <a href="https://www.watermelontools.com/pages/pricing">click here to buy</a></p>`
+    );
   }
   if (gitHubIssues) {
     addGitHubDailySummary({ gitHubIssues });
