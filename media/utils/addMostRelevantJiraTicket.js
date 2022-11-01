8a17aec8c9c3f259461d3aec000a46246944b786
Esteban Dalel R <estebandalelr@gmail.com>
2022-11-01 10:57:09 -0400

    Update addMostRelevantJiraTicket.js

--- media/utils/addMostRelevantJiraTicket.js
+++ media/utils/addMostRelevantJiraTicket.js
@@ -13,9 +13,12 @@ const paintTickets = (tickets) => {
       </a>
       <span style="color: green">${ticket?.fields?.status?.name}</span>
     </div>
-    <div class="Box-body">
-      ${ticket?.renderedFields?.description}
-    </div>
+   </div>
+    ${
+      ticket?.renderedFields?.description
+        ? `<div class="Box-body">${ticket?.renderedFields?.description}</div>`
+        : ""
+    }
     </div>
   `);
     }
