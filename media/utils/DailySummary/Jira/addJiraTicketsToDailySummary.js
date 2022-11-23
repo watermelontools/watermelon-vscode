0d92909fdc2393a13cb8f5ddfc98c520dee42342
Esteban Dalel R <estebandalelr@gmail.com>
2022-11-23 17:44:24 -0500

    Add comments to daily summary

--- media/utils/DailySummary/Jira/addJiraTicketsToDailySummary.js
+++ media/utils/DailySummary/Jira/addJiraTicketsToDailySummary.js
@@ -1,9 +1,11 @@
+import dateToHumanReadable from "../../dateToHumanReadable.js";
 const addJiraTicketsToDailySummary = (jiraTickets) => {
   $("#dailySummary").append(`<h3>Assigned Jira Tickets</h3>`);
   if (Array.isArray(jiraTickets)) {
     jiraTickets?.forEach((ticket) => {
+      console.log(ticket);
       $("#dailySummary").append(`
-      <div class="Box">
+      <div class="Box ${ticket.key}">
       <div class="Box-header d-flex flex-justify-between">
       <a href="${ticket.fields.priority.iconUrl.split("/images")[0]}/browse/${
         ticket?.key
@@ -22,6 +24,30 @@ const addJiraTicketsToDailySummary = (jiraTickets) => {
     </div>
     <br/>
     `);
+      if (Array.isArray(ticket?.comments)) {
+        {
+          ticket?.comments?.forEach((comment) => {
+            $(`.${ticket.key}`).append(`
+    <div class="Box">
+      <div class="Box-header d-flex">
+      <p class="pr-poster">
+        <img class='pr-author-img' src="${
+          comment?.updateAuthor?.avatarUrls["48x48"]
+        }" />
+        ${comment?.updateAuthor?.displayName}
+      </p>
+      <p class="pr-date">
+          on ${dateToHumanReadable(comment?.updated)}
+      </p>
+      </div>
+      <div class="Box-body">
+      ${comment.renderedBody}
+      </div>
+    </div>
+    `);
+          });
+        }
+      }
     });
   }
 };
