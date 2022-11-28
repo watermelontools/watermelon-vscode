6b0da3884582fe6e529a5a6aafae0dd3ca27adc1
Esteban Dalel R <estebandalelr@gmail.com>
2022-11-28 12:53:11 -0500

    Add posting on ticket

--- media/utils/DailySummary/Jira/addJiraTicketsToDailySummary.js
+++ media/utils/DailySummary/Jira/addJiraTicketsToDailySummary.js
@@ -50,21 +50,33 @@ const addJiraTicketsToDailySummary = (jiraTickets) => {
           });
         }
       }
-      $(`.${ticket.key}`)
-        .append(
-          `
-      <div class="d-flex flex-items-end flex-justify-end">
-       <button class="btn" type="button">Comment</button>
+      $(`.${ticket.key}`).append(
+        `
+      <div class="d-flex flex-items-end flex-justify-end" id="jira-comment-button-${ticket.key}">
+       <button class="btn" type="button" >Comment</button>
       </div>
       `
-        )
-        .on("click", function (e) {
-          sendMessage({
-            command: "jiraComment",
-            issueIdOrKey: ticket.key,
-            text: "hihi",
+      );
+      $(`#jira-comment-button-${ticket.key}`).on("click", function (e) {
+        $(`#jira-comment-button-${ticket.key}`).replaceWith(`
+         <div>
+         <span>Comment on ${ticket.key}</span>
+          <div>
+            <textarea id="jira-comment-${ticket.key}" placeholder="This input supports Markdown"></textarea>
+          </div>
+          <div class="d-flex flex-items-end flex-justify-end">
+           <button class="btn" type="button" id="jira-send-button-${ticket.key}">Send</button>
+          </div>
+         </div>
+         `);
+          $(`#jira-send-button-${ticket.key}`).on("click", function (e) {
+            sendMessage({
+              command: "jiraComment",
+              issueIdOrKey: ticket.key,
+              text: $(`#jira-comment-${ticket.key}`).val(),
+            });
           });
-        });
+      });
     });
   }
 };
