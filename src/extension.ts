1421ec53e95353f1f1f124f4502c47c491d019d4
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-18 09:29:28 -0500

    Add github to treeview

--- src/extension.ts
+++ src/extension.ts
@@ -144,6 +144,53 @@ export class WatermelonTreeDataProvider
       let sortedPRs = issuesWithTitlesAndGroupedComments?.sort(
         (a: any, b: any) => b.comments.length - a.comments.length
       );
+      let gitHubItems = sortedPRs.map((pr: any) => {
+        return new ContextItem(
+          pr.title,
+          vscode.TreeItemCollapsibleState.Collapsed,
+          `${pr.comments.length.toString()} comment${getPlural(
+            pr.comments.length
+          )}`,
+          {
+            command: WATERMELON_OPEN_LINK_COMMAND,
+            title: "View PR",
+            arguments: [pr.url],
+          },
+          pr.comments.map((comment: any) => {
+            return new ContextItem(
+              comment.user.login,
+              vscode.TreeItemCollapsibleState.None,
+              comment.created_at,
+              {
+                command: WATERMELON_OPEN_LINK_COMMAND,
+                title: "View comment",
+                arguments: [comment.userLink],
+              },
+              [
+                new ContextItem(
+                  comment.body,
+                  vscode.TreeItemCollapsibleState.None,
+                  dateToHumanReadable(comment.created_at),
+                  {
+                    command: WATERMELON_OPEN_LINK_COMMAND,
+                    title: "View comment",
+                    arguments: [comment.url],
+                  }
+                ),
+              ]
+            );
+          })
+        );
+      });
+      items.push(
+        new ContextItem(
+          "GitHub",
+          vscode.TreeItemCollapsibleState.Collapsed,
+          `${sortedPRs.length.toString()} PR${getPlural(sortedPRs.length)}`,
+          undefined,
+          gitHubItems
+        )
+      );
 
       // @ts-ignore
     } else {
