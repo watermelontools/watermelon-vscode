539beff7f1526a23066f57e6397ab136ab433233
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-19 15:12:20 -0500

    Allow changing Tree item logo

--- src/ContextItem.ts
+++ src/ContextItem.ts
@@ -7,18 +7,34 @@ export class ContextItem extends vscode.TreeItem {
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
     public readonly version: string,
     public readonly command?: vscode.Command,
-    children?: ContextItem[]
+    children?: ContextItem[],
+    public readonly logo?: string
   ) {
     super(label, collapsibleState);
 
     this.tooltip = `${this.label}-${this.version}`;
     this.description = this.version;
     this.children = children;
+    this.logo = logo;
   }
 
   iconPath = {
-    light: path.join(__filename, "..", "..", "images", "wmbw_bold_fill.svg"),
-    dark: path.join(__filename, "..", "..", "images", "wmbw_bold_fill.svg"),
+    light: path.join(
+      __filename,
+      "..",
+      "..",
+      "images",
+      "logos",
+      `${this.logo ? this.logo : "wmbw_bold_fill"}.svg`
+    ),
+    dark: path.join(
+      __filename,
+      "..",
+      "..",
+      "images",
+      "logos",
+      `${this.logo ? this.logo : "wmbw_bold_fill"}.svg`
+    ),
   };
 
   contextValue = "dependency";
