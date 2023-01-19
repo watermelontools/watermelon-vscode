cd6f2b7123dc53b3b9f028522678a6ee68951002
baristaGeek <estebanvargas94@gmail.com>
2023-01-19 17:07:26 -0500

    Change if else statements to a switch statement

--- src/utils/github/getPRsPerSHAS.ts
+++ src/utils/github/getPRsPerSHAS.ts
@@ -17,90 +17,94 @@ export default async function getPRsPerSHAS({
   repoSource: string;
 }) {
   let issuesItems;
+  let issues;
+  switch (repoSource) {
+    case "github.com":
+      issues = await axios
+        .post(`${backendURL}/api/github/getIssuesByCommits`, {
+          user: email,
+          repo,
+          owner,
+          commitList: shaArray,
+        })
+        .then((res) => res.data)
+        .catch((err) => {
+          let reporter = analyticsReporter();
+          let { message } = err;
+          reporter?.sendTelemetryException(err, { error: message });
+        });
+      issuesItems = issues.items;
+      break;
+    case "gitlab.com":
+      console.log("gitlab.com params: ", email, owner, repo, shaArray);
+      issues = await axios
+        .post(`${backendURL}/api/gitlab/getIssuesByCommits`, {
+          user: email,
+          owner: owner,
+          project_name: repo,
+          commitList: shaArray,
+        })
+        .then((res) => res.data)
+        .catch((err) => {
+          let reporter = analyticsReporter();
+          let { message } = err;
+          reporter?.sendTelemetryException(err, { error: message });
+        });
 
-  if (repoSource === "github.com") {
-    const issues = await axios
-      .post(`${backendURL}/api/github/getIssuesByCommits`, {
-        user: email,
-        repo,
-        owner,
-        commitList: shaArray,
-      })
-      .then((res) => res.data)
-      .catch((err) => {
-        let reporter = analyticsReporter();
-        let { message } = err;
-        reporter?.sendTelemetryException(err, { error: message });
-      });
-    issuesItems = issues.items;
-  } else if (repoSource === "gitlab.com") {
-    console.log("gitlab.com params: ", email, owner, repo, shaArray);
-    const issues = await axios
-      .post(`${backendURL}/api/gitlab/getIssuesByCommits`, {
-        user: email,
-        owner: owner,
-        project_name: repo,  
-        commitList: shaArray
-      })
-      .then((res) => res.data)
-      .catch((err) => {
-        let reporter = analyticsReporter();
-        let { message } = err;
-        reporter?.sendTelemetryException(err, { error: message });
-      });
+      // Here we are mapping the issues.items to match the github format
+      issuesItems = [
+        {
+          created_at: issues[0].created_at,
+          userImage: issues[0].author.avatar_url,
+          userLink: issues[0].author.web_url,
+          title: issues[0].title,
+          url: issues[0].web_url,
+          body: issues[0].description,
+          user: {
+            html_url: issues[0].author.web_url,
+            avatar_url: issues[0].author.avatar_url,
+            login: issues[0].author.username,
+          },
+          repository_url: issues[0].web_url,
+          state: issues[0].state,
+          draft: issues[0].draft,
+          number: issues[0].iid,
+          comments: [],
+        },
+      ];
+      break;
+    case "bitbucket.org":
+      issues = await axios
+        .post(`${backendURL}/api/bitbucket/getPullRequests`, {
+          userEmail: email,
+          repo_slug: repo, //id here, not name
+          commitHash: shaArray, //array vs singular, TODO
+          workspace: owner,
+        })
+        .then((res) => res.data)
+        .catch((err) => {
+          let reporter = analyticsReporter();
+          let { message } = err;
+          reporter?.sendTelemetryException(err, { error: message });
+        });
 
-    // Here we are mapping the issues.items to match the github format
-    issuesItems = [{
-      created_at: issues[0].created_at,
-      userImage: issues[0].author.avatar_url,
-      userLink: issues[0].author.web_url,
-      title: issues[0].title,
-      url: issues[0].web_url,
-      body: issues[0].description,
-      user: {
-        html_url: issues[0].author.web_url,
-        avatar_url: issues[0].author.avatar_url,
-        login: issues[0].author.username
-      },
-      repository_url: issues[0].web_url,
-      state: issues[0].state,
-      draft: issues[0].draft,
-      number: issues[0].iid,
-      comments: []
-    }];
-
-  } else if (repoSource === "bitbucket.org") {
-    const issues = await axios
-      .post(`${backendURL}/api/bitbucket/getPullRequests`, {
-        userEmail: email,
-        repo_slug: repo, //id here, not name
-        commitHash: shaArray, //array vs singular, TODO
-        workspace: owner,
-      })
-      .then((res) => res.data)
-      .catch((err) => {
-        let reporter = analyticsReporter();
-        let { message } = err;
-        reporter?.sendTelemetryException(err, { error: message });
-      });
-
-    // Here we are mapping the issues to match the github format
-    issuesItems = {
-      created_at: issues.items.created_at,
-      userImage: issues.items.author.avatar_url,
-      userLink: issues.items.author.web_url,
-      title: issues.items.title,
-      url: issues.items.web_url,
-      body: issues.items.description,
-      user: {
-        html_url: issues.items.author.web_url,
-        avatar_url: issues.items.author.avatar_url,
-      },
-      repository_url: issues.items.web_url,
-      state: issues.items.state,
-      draft: issues.items.draft,
-      number: issues.items.iid,
-    };
+      // Here we are mapping the issues to match the github format
+      issuesItems = {
+        created_at: issues.items.created_at,
+        userImage: issues.items.author.avatar_url,
+        userLink: issues.items.author.web_url,
+        title: issues.items.title,
+        url: issues.items.web_url,
+        body: issues.items.description,
+        user: {
+          html_url: issues.items.author.web_url,
+          avatar_url: issues.items.author.avatar_url,
+        },
+        repository_url: issues.items.web_url,
+        state: issues.items.state,
+        draft: issues.items.draft,
+        number: issues.items.iid,
+      };
   }
 
   return issuesItems;
